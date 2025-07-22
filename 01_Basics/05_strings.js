ed4d771780cfe7f22dface0c88059ff322b7b325
Sachin Jaiswal <101462110+SachJaiswal@users.noreply.github.com>
2025-07-22 14:11:19 +0000

    Strings completed

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,35 @@
+let name="Sachin"
+let repocount=50
+
+// String Interpolation
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`)
+
+
+const gameName = new String('sachin-sj-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://Sachin.com/sachin%20jaiswal"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
