78409fa7a504d436c7bfbda8f17c1ae9455257ca
Sachin Jaiswal <101462110+SachJaiswal@users.noreply.github.com>
2025-07-23 03:10:47 +0000

    array half

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,14 @@
+//array
+
+const arr=[1,4,5,6,7,8]    // 0th based indexing 
+
+//array create Shallow Copy    share the same referene points
+
+
+console.log(arr[0])
+
+const arr1 = new Array(10,45,65,778,45);
+console.log(arr1[0])
+console.log(arr1.length)
+
+
