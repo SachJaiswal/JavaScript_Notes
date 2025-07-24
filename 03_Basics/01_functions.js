72fb7c30c650adc26bee41eea703dfe5df6cf6d4
Sachin Jaiswal <101462110+SachJaiswal@users.noreply.github.com>
2025-07-24 16:01:49 +0000

    Finished FUNCTION

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -17,22 +17,22 @@ function saymyname(){
 // saymyname()
 
 
-// function add2num(a,b)  //Parameters
-// {
-//     return a**b
+function add2num(a,b)  //Parameters
+{
+    return a**b
 
-// }
+}
 // console.log(add2num(5,5)) // add2num(5,5) input  is Arguments
 
 
 
 
-// function add2num(a,b)  //Parameters
-// {
-//     return a**b
-//     console.log("Sachin")   // Unreachable code detected.   After return no code is executed     
+function add2num(a,b)  //Parameters
+{
+    return a**b
+    console.log("Sachin")   // Unreachable code detected.   After return no code is executed     
 
-// }
+}
 
 // const result = add2num(5,5)
 // console.log(result) // add2num(5,5) input  is Arguments
@@ -48,4 +48,54 @@ function loginUserName(username){
 }
 
 // console.log(loginUserName("Sachin"))
-console.log(loginUserName())    // Application Login By the Name undefined 
+// console.log(loginUserName())    // Application Login By the Name undefined 
+
+
+/* --------------------------------------  */
+
+
+//  ...variable    is called the rest operator we can take multiple arguments 
+function calculateCartPrice1(...num1){
+    return num1
+}
+
+//  console.log(calculateCartPrice(100,233,700))    // Output :  [ 100, 233, 700 ]
+
+
+
+function calculateCartPrice2(var1,var2,...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice2(100,233,700,2000))   
+
+// Output : [ 700, 2000 ]    var1 = 100, var2 = 233 and rest into num1 
+
+
+const user = {
+    userName : "Sachin",
+    age : 21
+}
+
+function handleObject(anyObjects){
+    return `My name is ${anyObjects.userName} and age is ${anyObjects.age} `
+}
+
+console.log(handleObject(user))
+
+// passed object directly 
+console.log(
+ handleObject({
+    userName : "Jaiswal",
+    age:41
+}))
+
+
+// Passed Array
+
+const arr=[100,200,300,400]
+
+function returnSecondValue(anyObjects){
+    return anyObjects[1]
+}
+console.log(returnSecondValue(arr));
