1a7453d88b3a451479031f8ba68a5ab9bb39d871
Sachin Jaiswal <101462110+SachJaiswal@users.noreply.github.com>
2025-07-25 07:02:37 +0000

    Basic loop completed

---
+++ 05_Iterations/do_while.js
@@ -0,0 +1,28 @@
+// While Loop
+
+// let index=1
+// let table=7
+
+// while (index <= 10) {
+//     console.log(` ${table} * ${index} = ${table * index} `);
+//     index++;
+// }
+// table++;
+
+
+// const arr =['A','B','C']
+// let i =0
+// while(i<=arr.length){
+//     console.log(arr[i])
+//     i++
+// }
+
+let score=1
+do {
+    console.log(`${score}`);
+    score++
+    
+} while (score<=10);
+
+
+
