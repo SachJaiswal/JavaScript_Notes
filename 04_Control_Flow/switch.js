4247ec7934ef4c5e87f758d5510012343806befc
Sachin Jaiswal <101462110+SachJaiswal@users.noreply.github.com>
2025-07-25 04:00:30 +0000

    Completed Control Flow

---
+++ 04_Control_Flow/switch.js
@@ -0,0 +1,30 @@
+// const key ="v2"
+// switch(key){
+//     case v1:
+//         break
+//     case v2:
+//         break
+//     default:
+//         break
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
